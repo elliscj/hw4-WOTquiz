7af1351e8ab71f9f1795b7510b8af77d8664df1c
Cam Ellis <elliscj@colorado.edu>
2021-11-09 23:19:59 -0700

    html and css written

---
+++ Assets/js/script.js
@@ -0,0 +1,17 @@
+var answers = {
+  father: "Janduin",
+  tainted: "Dark One",
+  terangreal: "the Acess Keys",
+  betrayer: "Ishamael",
+  halfBrother: "Galad",
+  malkieri: "Golden Crane",
+};
+
+var user = {
+  father: "",
+  tainted: "",
+  terangreal: "",
+  betrayer: "",
+  halfBrother: "",
+  malkieri: "",
+};
