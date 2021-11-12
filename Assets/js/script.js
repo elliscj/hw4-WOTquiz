db0450b939ff86099a6ec6c4e3d790003b67091f
Cam Ellis <elliscj@colorado.edu>
2021-11-11 17:28:25 -0700

    function rendering each question and answer is properly working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,80 +2,117 @@ var quizBoxEl = $("#quiz-box");
 var timeEl = $("#time-left");
 var highScoreEl = $("#high-scores");
 var buttonEl = $(".button");
-var quizQuestions = {
-  question1: "Who is Rand al Thor's Father?",
-  question2: "Who tainted the male half of the One Power?",
-  question3: "Which of the following is a Terangreal?",
-  question4: "Who's name means 'Betrayer of Hope' in the old tounge?",
-  question5: "Who is Rand's half brother?",
-  question6: "What is on the Malkieri flag?",
-};
+var answerButton = $("#options");
+var startButtonEl = $("#start-button");
+var currentQuestionIndex = 0;
+var questionTextEl = $("#question");
+var optionAEl = $("#optionA");
+var optionBEl = $("#optionB");
+var optionCEl = $("#optionC");
+var optionDEl = $("#optionD");
 
-var question1options = [
-  "Janduin",
-  "Tam",
-  "Lews Therin Telamon",
-  "King Aemon al Caar al Thorin",
+//array of objects title question key value pairs for querstion and answers
+var quizQuestions = [
+  {
+    title: "Who is Rand al Thor's Father?",
+    userChoices: [
+      "Janduin",
+      "Tam",
+      "Lews Therin Telamon",
+      "King Aemon al Caar al Thorin",
+    ],
+    answer: "Janduin",
+  },
+
+  {
+    title: "Who tainted the male half of the One Power?",
+    userChoices: [
+      "Lews Therin Telamon",
+      "The Dark One",
+      "Ishamael",
+      "Raolin Darksbane",
+    ],
+    answer: "The Dark One",
+  },
+
+  {
+    title: "Which of the following is a Terangreal?",
+    userChoices: [
+      "Callandor",
+      "Choedan Kal",
+      "Vora's Sa'angreal (the White Rod)",
+      "the Access Keys",
+    ],
+    answer: "the Access Keys",
+  },
+
+  {
+    title: "Who's name means 'Betrayer of Hope' in the old tounge?",
+    userChoices: ["Ba'alzamon", "Ishamael", "Balthamel", "Demandred"],
+    answer: "Ishamael",
+  },
+
+  {
+    title: "Who is Rand's half brother?",
+    userChoices: ["Gawan", "Galad", "Lan", "Uno"],
+    answer: "Galad",
+  },
+
+  {
+    title: "What is on the Malkieri flag?",
+    userChoices: ["Dragon", "Red Eagle", "Red Hand", "Golden Crane"],
+    answer: "Golden Crane",
+  },
 ];
 
-var question2options = [
-  "Lews Therin Telamon",
-  "The Dark One",
-  "Ishamael",
-  "Raolin Darksbane",
-];
-
-var question3options = [
-  "Callandor",
-  "Choedan Kal",
-  "Vora's Sa'angreal (the White Rod)",
-  "the Access Keys",
-];
-
-var question4options = ["Ba'alzamon", "Ishamael", "Balthamel", "Demandred"];
-
-var question5options = ["Gawan", "Galad", "Lan", "Uno"];
-
-var question6options = ["Dragon", "Red Eagle", "Red Hand", "Golden Crane"];
-
-var answers = {
-  father: "Janduin",
-  tainted: "The Dark One",
-  terangreal: "the Acess Keys",
-  betrayer: "Ishamael",
-  halfBrother: "Galad",
-  malkieri: "Golden Crane",
-};
-
+//create variable to save info json parse empty array with score and initials
+//check local storage pull local storage if not there make empty array the stringify and save
+//var index = 0 incriment the index nnumber and call the object
+//
 var user = {
-  father: "",
-  tainted: "",
-  terangreal: "",
-  betrayer: "",
-  halfBrother: "",
-  malkieri: "",
-  initials: "",
-  score: "0",
+  initials: userinitials,
+  score: userScore,
 };
-
+var userinitials;
+var userScore = 0;
 var secondsLeft = 60;
 
-buttonEl.hide();
-highScoreEl.hide();
+var highScores = JSON.parse(localStorage.getItem("scores")) || [];
+console.log(highScores);
+
+// buttonEl.hide();
+// highScoreEl.hide();
+
+answerButton.on("click", function (event) {
+  if (quizQuestions[currentQuestionIndex].answer === event.target.outerText) {
+    userScore += 5;
+    console.log(userScore);
+  } else {
+    secondsLeft -= 5;
+  }
+
+  console.log(event);
+
+  currentQuestionIndex++;
+
+  renderQuestion();
+});
+
+function renderQuestion() {
+  questionTextEl.text(quizQuestions[currentQuestionIndex].title);
+  optionAEl.text(quizQuestions[currentQuestionIndex].userChoices[0]);
+  optionBEl.text(quizQuestions[currentQuestionIndex].userChoices[1]);
+  optionCEl.text(quizQuestions[currentQuestionIndex].userChoices[2]);
+  optionDEl.text(quizQuestions[currentQuestionIndex].userChoices[3]);
+}
 
 function startQuiz() {
-  //start timer
+  renderQuestion();
 
   timer();
-
-  user.initials = prompt(
-    "Please enter your initials so you can post your high score."
-  );
 }
 
 buttonEl.on("click", function () {});
-console.log(user);
-console.log(question3options);
 
 function timer() {
   // Sets interval in variable
@@ -95,6 +132,10 @@ function timer() {
 function timesUp() {
   timeEl.textContent = "0";
   alert("Times Up!");
+  userInitials = prompt(
+    "Please enter your initials so you can post your high score."
+  );
 }
 
-quizBoxEl.on("click", startQuiz);
+startButtonEl.on("click", startQuiz);
+console.log(user);
